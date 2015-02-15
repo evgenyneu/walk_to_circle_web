e5a88f73788d65720c165304718dea8e6a81f9e1
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2015-02-15 11:20:55 +1100

    Set gulp serve

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,31 @@
 (function () {
   'use strict';
-  var gulp = require('gulp');
+  var gulp = require('gulp'),
+      connect = require('gulp-connect'),
+      open = require('gulp-open');
+
+  var paths = {
+    temp: '_tmp'
+  };
+
+  gulp.task('connect', function() {
+    connect.server({
+      root: ['app', paths.temp],
+      port: 1336,
+      livereload: true
+    });
+  });
+
+  gulp.task('watch', function() {
+    gulp.watch(['app/*.html', 'app/scss/**/*', 'app/js/**/*'], ['html']);
+  });
+
+  gulp.task('open', ['connect', 'watch'], function(){
+    gulp.src('app/index.html')
+    .pipe(open('', { url: 'http://localhost:1336' }));
+  });
+
+  gulp.task('serve', ['open']);
 
   gulp.task('default', function() {
     // place code for your default task here
